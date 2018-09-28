ac00459ec128642d1a0621d70e8a56d52224efc5
Matthieu Dubois <m-dubois@users.noreply.github.com>
2018-09-28 09:57:00 +0200

    added position to mower building

--- src/main/java/org/matt/kata/mod/Mower.java
+++ src/main/java/org/matt/kata/mod/Mower.java
@@ -2,7 +2,20 @@ package org.matt.kata.mod;
 
 public class Mower {
 
+    public static final int DEFAULT_X_POSITION = 0;
+    public static final int DEFAULT_Y_POSITION = 0;
+
+    private Position position = null;
+
+    public Mower() {
+        this.position = new Position(DEFAULT_X_POSITION, DEFAULT_Y_POSITION);
+    }
+
+    public Mower(int x, int y) {
+        this.position = new Position(x, y);
+    }
+
     public Object getPosition() {
-        return new Position();
+        return this.position;
     }
 }
