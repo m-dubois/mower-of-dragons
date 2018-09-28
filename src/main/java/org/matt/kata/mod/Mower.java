91e2c9edefcf1f7b75bc2ee47b1ba21f53a0a589
Matthieu Dubois <m-dubois@users.noreply.github.com>
2018-09-28 12:54:05 +0200

    added direction to mower building

--- src/main/java/org/matt/kata/mod/Mower.java
+++ src/main/java/org/matt/kata/mod/Mower.java
@@ -4,15 +4,24 @@ public class Mower {
 
     public static final int DEFAULT_X_POSITION = 0;
     public static final int DEFAULT_Y_POSITION = 0;
+    public static final Direction DEFAULT_DIRECTION = Direction.NORTH;
 
     private Position position = null;
+    private Direction direction = null;
 
     public Mower() {
         this.position = new Position(DEFAULT_X_POSITION, DEFAULT_Y_POSITION);
+        this.direction = DEFAULT_DIRECTION;
     }
 
     public Mower(int x, int y) {
         this.position = new Position(x, y);
+        this.direction = DEFAULT_DIRECTION;
+    }
+
+    public Mower(int x, int y, Direction direction) {
+        this.position = new Position(x, y);
+        this.direction = direction;
     }
 
     public Object getPosition() {
@@ -20,6 +29,10 @@ public class Mower {
     }
 
     public Object getDirection() {
-        return Direction.NORTH;
+        return this.direction;
+    }
+
+    public void setDirection(Direction direction) {
+        this.direction = direction;
     }
 }
