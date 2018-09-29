e9b7be31a1700b542ca2b5794156f9b9ab880fce
Matthieu Dubois <matthieu.dubois@owood.fr>
2018-09-29 23:38:20 +0200

    added lawn can have a mower

--- src/main/java/org/matt/kata/mod/Lawn.java
+++ src/main/java/org/matt/kata/mod/Lawn.java
@@ -1,13 +1,18 @@
 package org.matt.kata.mod;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Lawn {
 
     private int maxPositionX;
     private int maxPositionY;
+    private List<Mower> mowers;
 
     public Lawn(int maxPositionX, int maxPositionY) {
         this.maxPositionX = maxPositionX;
         this.maxPositionY = maxPositionY;
+        this.mowers = new ArrayList<>();
     }
 
     public int getMaxPositionY() {
@@ -16,5 +21,9 @@ public class Lawn {
 
     public int getMaxPositionX() {
         return maxPositionX;
+    }
+
+    public void addMower(Mower mower) {
+        this.mowers.add(mower);
     }
 }
