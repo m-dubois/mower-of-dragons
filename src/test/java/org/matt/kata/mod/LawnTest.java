9d51b9864a57ed6919ba887204921fd6513994fb
Matthieu Dubois <matthieu.dubois@owood.fr>
2018-09-29 23:36:03 +0200

    [test] lawn: lawn can have a mower

--- src/test/java/org/matt/kata/mod/LawnTest.java
+++ src/test/java/org/matt/kata/mod/LawnTest.java
@@ -13,4 +13,12 @@ public class LawnTest {
         assertEquals(2, aLawn.getMaxPositionY());
     }
 
+    @Test
+    public void aLawnIsAddedAMower() {
+        Lawn aLawn = new Lawn(3, 2);
+        assertEquals(3, aLawn.getMaxPositionX());
+        assertEquals(2, aLawn.getMaxPositionY());
+        aLawn.addMower(new Mower(0, 0));
+    }
+
 }
