847b3d7162989eb2614c2c3c2310c85b56e93a50
Matthieu Dubois <m-dubois@users.noreply.github.com>
2018-10-01 15:21:49 +0200

    implemented the concrete command MoveForwardCommand

--- src/main/java/org/matt/kata/mod/MoveForwardCommand.java
+++ src/main/java/org/matt/kata/mod/MoveForwardCommand.java
@@ -2,11 +2,14 @@ package org.matt.kata.mod;
 
 public class MoveForwardCommand implements Command {
 
+    private Mower mower;
+
     public MoveForwardCommand(Mower aMower) {
+        this.mower = aMower;
     }
 
     @Override
     public void execute() {
-
+        this.mower.movesForward();
     }
 }
