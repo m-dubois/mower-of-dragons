653d33a1be43467ee6eb22436cd74fb76f2ddf4e
Matthieu Dubois <matthieu.dubois@owood.fr>
2018-10-05 23:12:34 +0200

    [test] service: fixed lawn service

--- src/test/java/org/matt/kata/mob/domain/service/LawnServiceTest.java
+++ src/test/java/org/matt/kata/mob/domain/service/LawnServiceTest.java
@@ -3,13 +3,15 @@ package org.matt.kata.mob.domain.service;
 import org.junit.Test;
 import org.matt.kata.mod.domain.model.Lawn;
 
+import static org.junit.Assert.assertEquals;
+
 public class LawnServiceTest {
 
     @Test
     public Lawn createLawn() {
 
-        LawnService lawnService = new LawnService();
-        Lawn aMLawn = lawnService.createLawn(8, 5);
+        LawnService lawnService = new LawnServiceImpl();
+        Lawn aLawn = lawnService.createLawn(8, 5);
 
         assertEquals(8, aLawn.getMaxPositionX());
         assertEquals(5, aLawn.getMaxPositionY());
