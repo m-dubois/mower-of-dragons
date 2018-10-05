b8fe502f146e41a39ce0114ce2c0c499f19bc6fd
Matthieu Dubois <matthieu.dubois@owood.fr>
2018-10-05 23:30:28 +0200

    fixed various problems in lawn service test
    too tired :/

--- src/test/java/org/matt/kata/mod/domain/service/LawnServiceTest.java
+++ src/test/java/org/matt/kata/mod/domain/service/LawnServiceTest.java
@@ -1,14 +1,15 @@
-package org.matt.kata.mob.domain.service;
+package org.matt.kata.mod.domain.service;
 
 import org.junit.Test;
 import org.matt.kata.mod.domain.model.Lawn;
+import org.matt.kata.mod.infrastructure.service.LawnServiceImpl;
 
 import static org.junit.Assert.assertEquals;
 
 public class LawnServiceTest {
 
     @Test
-    public Lawn createLawn() {
+    public void createLawn() {
 
         LawnService lawnService = new LawnServiceImpl();
         Lawn aLawn = lawnService.createLawn(8, 5);
