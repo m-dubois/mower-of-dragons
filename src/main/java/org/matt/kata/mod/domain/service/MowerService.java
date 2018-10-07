cc733e3f72ddc5518ca7678a81dd9b28ec74318a
Matthieu Dubois <matthieu.dubois@owood.fr>
2018-10-07 23:48:44 +0200

    added lawn to createMower in mower service

--- src/main/java/org/matt/kata/mod/domain/service/MowerService.java
+++ src/main/java/org/matt/kata/mod/domain/service/MowerService.java
@@ -1,8 +1,9 @@
 package org.matt.kata.mod.domain.service;
 
 import org.matt.kata.mod.domain.model.Direction;
+import org.matt.kata.mod.domain.model.Lawn;
 import org.matt.kata.mod.domain.model.Mower;
 
 public interface MowerService {
-    Mower createMower(int i, int i1, Direction east);
+    Mower createMower(int x, int y, Direction direction, Lawn lawn);
 }
--- src/main/java/org/matt/kata/mod/infrastructure/service/MowerServiceImpl.java
+++ src/main/java/org/matt/kata/mod/infrastructure/service/MowerServiceImpl.java
@@ -1,12 +1,13 @@
 package org.matt.kata.mod.infrastructure.service;
 
 import org.matt.kata.mod.domain.model.Direction;
+import org.matt.kata.mod.domain.model.Lawn;
 import org.matt.kata.mod.domain.model.Mower;
 import org.matt.kata.mod.domain.service.MowerService;
 
 public class MowerServiceImpl implements MowerService {
     @Override
-    public Mower createMower(int x, int y, Direction direction) {
-        return new Mower(x, y, direction, null);
+    public Mower createMower(int x, int y, Direction direction, Lawn aLawn) {
+        return new Mower(x, y, direction, aLawn);
     }
 }
