5867f37609da0fb8447520f6b484fe3bee061af3
Matthieu Dubois <matthieu.dubois@owood.fr>
2018-10-08 00:00:59 +0200

    [test] service: mower service - enqueue a move command

--- src/test/java/org/matt/kata/mod/domain/service/MowerServiceTest.java
+++ src/test/java/org/matt/kata/mod/domain/service/MowerServiceTest.java
@@ -1,10 +1,13 @@
 package org.matt.kata.mod.domain.service;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.matt.kata.mod.domain.model.Direction;
 import org.matt.kata.mod.domain.model.Lawn;
 import org.matt.kata.mod.domain.model.Mower;
 import org.matt.kata.mod.domain.model.Position;
+import org.matt.kata.mod.domain.model.commands.Command;
+import org.matt.kata.mod.domain.model.commands.MoveForwardCommand;
 import org.matt.kata.mod.infrastructure.service.MowerServiceImpl;
 
 import static org.junit.Assert.assertEquals;
@@ -22,6 +25,14 @@ public class MowerServiceTest {
 
         assertEquals(new Position(4, 7), aMower.getPosition());
         assertEquals(Direction.EAST, aMower.getDirection());
+    }
+
+    @Test
+    public void enqueueMoveCommand() {
+
+        Mower aMower = new Mower(3, 2, Direction.SOUTH, new Lawn(8, 6));
+        Command command = new MoveForwardCommand(aMower);
+        Assert.assertEquals(1, aMower.getCommands().size());
     }
 
 }
