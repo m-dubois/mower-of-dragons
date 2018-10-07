58a5c29d3180a56d13f49309092a0aa0dc15832a
Matthieu Dubois <matthieu.dubois@owood.fr>
2018-10-08 00:00:07 +0200

    added lawn in createMower service

--- src/test/java/org/matt/kata/mod/domain/service/MowerServiceTest.java
+++ src/test/java/org/matt/kata/mod/domain/service/MowerServiceTest.java
@@ -2,6 +2,7 @@ package org.matt.kata.mod.domain.service;
 
 import org.junit.Test;
 import org.matt.kata.mod.domain.model.Direction;
+import org.matt.kata.mod.domain.model.Lawn;
 import org.matt.kata.mod.domain.model.Mower;
 import org.matt.kata.mod.domain.model.Position;
 import org.matt.kata.mod.infrastructure.service.MowerServiceImpl;
@@ -13,8 +14,11 @@ public class MowerServiceTest {
     @Test
     public void createMower() {
 
-        MowerService mowerService= new MowerServiceImpl();
-        Mower aMower = mowerService.createMower(4, 7, Direction.EAST);
+        MowerService mowerService = new MowerServiceImpl();
+
+        Lawn aLawn = new Lawn(8, 12);
+
+        Mower aMower = mowerService.createMower(4, 7, Direction.EAST, aLawn);
 
         assertEquals(new Position(4, 7), aMower.getPosition());
         assertEquals(Direction.EAST, aMower.getDirection());
