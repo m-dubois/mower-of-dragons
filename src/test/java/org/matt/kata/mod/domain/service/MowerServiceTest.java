c078fc9f620661e066ce5c63a86924cbd633e071
Matthieu Dubois <matthieu.dubois@owood.fr>
2018-10-07 23:03:59 +0200

    [test] service: mower service - create a mower

---
+++ src/test/java/org/matt/kata/mod/domain/service/MowerServiceTest.java
@@ -0,0 +1,22 @@
+package org.matt.kata.mod.domain.service;
+
+import org.junit.Test;
+import org.matt.kata.mod.domain.model.Direction;
+import org.matt.kata.mod.domain.model.Mower;
+import org.matt.kata.mod.domain.model.Position;
+
+import static org.junit.Assert.assertEquals;
+
+public class MowerServiceTest {
+
+    @Test
+    public void createMower() {
+
+        MowerService mowerService= new MowerServiceImpl();
+        Mower aMower = mowerService.createMower(4, 7, Direction.EAST);
+
+        assertEquals(new Position(4, 7), aMower.getPosition());
+        assertEquals(Direction.EAST, aMower.getDirection());
+    }
+
+}
