87bca80db6dab032f5dcf5fe566c61786375a402
Matthieu Dubois <matthieu.dubois@owood.fr>
2018-10-09 22:24:12 +0200

    [test] service: lawn service - add a mower to a lawn

--- src/test/java/org/matt/kata/mod/domain/service/LawnServiceTest.java
+++ src/test/java/org/matt/kata/mod/domain/service/LawnServiceTest.java
@@ -2,6 +2,7 @@ package org.matt.kata.mod.domain.service;
 
 import org.junit.Test;
 import org.matt.kata.mod.domain.model.Lawn;
+import org.matt.kata.mod.domain.model.Mower;
 import org.matt.kata.mod.infrastructure.service.LawnServiceImpl;
 
 import static org.junit.Assert.assertEquals;
@@ -18,6 +19,19 @@ public class LawnServiceTest {
         assertNotNull(aLawn);
         assertEquals(8, aLawn.getMaxPositionX());
         assertEquals(5, aLawn.getMaxPositionY());
+    }
+
+    @Test
+    public void addMowerToLawn() {
+
+        LawnService lawnService = new LawnServiceImpl();
+        Lawn aLawn = lawnService.createLawn(8, 5);
+
+        Mower aMower = new Mower();
+        lawnService.addMowerToLawn(aMower, aLawn);
+
+        assertNotNull(aLawn.getMowers());
+        assertEquals(1, aLawn.getMowers().size());
     }
 
 }
