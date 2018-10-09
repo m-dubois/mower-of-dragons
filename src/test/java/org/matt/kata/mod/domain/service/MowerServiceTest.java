74d7d4bb719a3b432d50cef33937031c017b5861
Matthieu Dubois <matthieu.dubois@owood.fr>
2018-10-09 22:53:57 +0200

    [test] service: mower service - enqueue a turn left command

--- src/test/java/org/matt/kata/mod/domain/service/MowerServiceTest.java
+++ src/test/java/org/matt/kata/mod/domain/service/MowerServiceTest.java
@@ -1,24 +1,31 @@
 package org.matt.kata.mod.domain.service;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.matt.kata.mod.domain.model.Direction;
 import org.matt.kata.mod.domain.model.Lawn;
 import org.matt.kata.mod.domain.model.Mower;
 import org.matt.kata.mod.domain.model.Position;
 import org.matt.kata.mod.domain.model.commands.MoveForwardCommand;
+import org.matt.kata.mod.domain.model.commands.TurnLeftCommand;
 import org.matt.kata.mod.infrastructure.service.MowerServiceImpl;
 
 import static org.junit.Assert.assertEquals;
 
 public class MowerServiceTest {
 
+    private Lawn aLawn;
+
+    @Before
+    public void setup() {
+        aLawn = new Lawn(8, 12);
+    }
+
     @Test
     public void createMower() {
 
         MowerService mowerService = new MowerServiceImpl();
-
-        Lawn aLawn = new Lawn(8, 12);
 
         Mower aMower = mowerService.createMower(4, 7, Direction.EAST);
 
@@ -29,12 +36,22 @@ public class MowerServiceTest {
     @Test
     public void enqueueMoveCommand() {
         MowerService mowerService = new MowerServiceImpl();
-        Lawn aLawn = new Lawn(8, 12);
         Mower aMower = mowerService.createMower(4, 7, Direction.EAST);
         mowerService.enqueueMoveForwardCommand(aMower);
 
         Assert.assertEquals(1, aMower.getCommands().size());
         Assert.assertEquals(MoveForwardCommand.class, aMower.getCommands().get(0).getClass());
     }
+
+    @Test
+    public void enqueueTurnLeftCommand() {
+        MowerService mowerService = new MowerServiceImpl();
+        Mower aMower = mowerService.createMower(4, 7, Direction.EAST);
+        mowerService.enqueueTurnLeftCommand(aMower);
+
+        Assert.assertEquals(1, aMower.getCommands().size());
+        Assert.assertEquals(TurnLeftCommand.class, aMower.getCommands().get(0).getClass());
+    }
+
 
 }
