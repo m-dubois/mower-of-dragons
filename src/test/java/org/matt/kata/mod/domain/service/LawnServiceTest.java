5b1284498286b2ec122c0cef180ad0e26894b439
Matthieu Dubois <matthieu.dubois@owood.fr>
2018-10-09 22:19:36 +0200

    added tests in lawn service

--- src/test/java/org/matt/kata/mod/domain/service/LawnServiceTest.java
+++ src/test/java/org/matt/kata/mod/domain/service/LawnServiceTest.java
@@ -5,6 +5,7 @@ import org.matt.kata.mod.domain.model.Lawn;
 import org.matt.kata.mod.infrastructure.service.LawnServiceImpl;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 public class LawnServiceTest {
 
@@ -14,6 +15,7 @@ public class LawnServiceTest {
         LawnService lawnService = new LawnServiceImpl();
         Lawn aLawn = lawnService.createLawn(8, 5);
 
+        assertNotNull(aLawn);
         assertEquals(8, aLawn.getMaxPositionX());
         assertEquals(5, aLawn.getMaxPositionY());
     }
