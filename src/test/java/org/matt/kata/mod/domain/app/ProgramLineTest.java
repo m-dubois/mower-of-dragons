abf2155f6a31ea34014a8e97b2ae43b78151101a
Matthieu Dubois <matthieu.dubois@owood.fr>
2018-10-15 22:28:05 +0200

    [test] program line parser

---
+++ src/test/java/org/matt/kata/mod/domain/app/ProgramLineTest.java
@@ -0,0 +1,17 @@
+package org.matt.kata.mod.domain.app;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.matt.kata.mod.domain.model.Lawn;
+
+public class ProgramLineTest {
+
+    @Test
+    public void firstProgramLineTest() {
+
+        String line = "5 5\n";
+        Lawn lawn = LineParser.parseLawnCoordinates(line);
+        Assert.assertEquals(new Lawn(5, 5), lawn);
+    }
+
+}
