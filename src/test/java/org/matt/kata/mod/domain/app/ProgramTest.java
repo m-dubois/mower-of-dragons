f94fa9bb8319416181137b4ce8dd2ef2f98641f9
Matthieu Dubois <m-dubois@users.noreply.github.com>
2018-10-15 14:45:27 +0200

    [test] app: program test - program definition

---
+++ src/test/java/org/matt/kata/mod/domain/app/ProgramTest.java
@@ -0,0 +1,31 @@
+package org.matt.kata.mod.domain.app;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.matt.kata.mod.domain.model.Direction;
+import org.matt.kata.mod.domain.model.Lawn;
+import org.matt.kata.mod.domain.model.Mower;
+import org.matt.kata.mod.domain.model.commands.Command;
+import org.matt.kata.mod.domain.model.commands.MoveForwardCommand;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProgramTest {
+
+    @Test
+    public void aProgramDefinesLawnMowersAndCommands() {
+
+        Program program = new Program();
+        program.setMaxCoordinates(6, 9);
+
+        Mower mower = new Mower(0, 0, Direction.NORTH);
+        List<Command> commandList = new ArrayList<>();
+        commandList.add(new MoveForwardCommand());
+        program.addMower(mower, commandList);
+
+        Assert.assertEquals(new Lawn(6, 9), program.getLawn());
+        Assert.assertEquals(program.getMowersCount(), 1);
+        Assert.assertEquals(program.getMowerCommands(0).size(), 1);
+    }
+}
