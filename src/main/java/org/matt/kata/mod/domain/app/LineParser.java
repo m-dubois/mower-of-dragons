f93563a1ac0702de04483ca075b15496dc745194
Matthieu Dubois <matthieu.dubois@owood.fr>
2018-10-15 23:21:41 +0200

    added parseMowerPositionAndDirection

--- src/main/java/org/matt/kata/mod/domain/app/LineParser.java
+++ src/main/java/org/matt/kata/mod/domain/app/LineParser.java
@@ -1,12 +1,17 @@
 package org.matt.kata.mod.domain.app;
 
+import org.matt.kata.mod.domain.model.Direction;
 import org.matt.kata.mod.domain.model.Lawn;
+import org.matt.kata.mod.domain.model.Mower;
 import org.matt.kata.mod.domain.service.LawnService;
+import org.matt.kata.mod.domain.service.MowerService;
 import org.matt.kata.mod.infrastructure.service.LawnServiceImpl;
+import org.matt.kata.mod.infrastructure.service.MowerServiceImpl;
 
 class LineParser {
 
     private static final String COORDINATES_SEPARATOR = " ";
+    private static final String POSITION_AND_DIRECTION_SEPARATOR = " ";
 
     static Lawn parseLawnCoordinates(String coordinatesline) throws ProgramException {
 
@@ -25,5 +30,27 @@ class LineParser {
         } else {
             throw new ProgramException("Coordinates are made of 2 integers; found ", coords.length);
         }
+    }
+
+    public static Mower parseMowerPositionAndDirection(String positionAndDirectionLine) throws ProgramException {
+
+        String line = Utils.removeEndingNewLineIfNeeded(positionAndDirectionLine);
+
+        String[] posAndDir = line.split(POSITION_AND_DIRECTION_SEPARATOR);
+
+        if (posAndDir.length == 3) {
+
+            int x = Integer.parseInt(posAndDir[0]);
+            int y = Integer.parseInt(posAndDir[1]);
+            String dir = posAndDir[2];
+            Direction direction = DirectionParser.parse(dir);
+
+            MowerService mowerService = new MowerServiceImpl();
+            return mowerService.createMower(x, y, direction);
+
+        } else {
+            throw new ProgramException("Coordinates are made of 2 integers; found ", posAndDir.length);
+        }
+
     }
 }
